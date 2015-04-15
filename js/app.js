9697bdc2efc79e4018507775e7337a87041f6455
Justin Golownia <jmgolownia@gmail.com>
2015-04-14 22:23:02 -0500

    fixed chart js and responsive errors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// Doughnut Chart for Weekly Goals
+var fundingTotal = [
+  {
+    value: 2107,
+    color:"#9822C0"
+  },
+  {
+    value : 6393,
+    color : "#E2EAE9"
+  }
+]
+var options = {
+  percentageInnerCutout : 50
+};
+var fundChart = $("#fundingTotal").get(0).getContext("2d");
+var myfundChart = new Chart(fundChart).Doughnut(fundingTotal, options);
